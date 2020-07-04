b6275d7db7cc176d517df814bdab753a2733f0d2
edrapac <emerson.drapac@mnsu.edu>
2020-07-04 13:40:39 -0500

    added arg parsing and error handling for searches that return a single page

--- dork.py
+++ dork.py
@@ -3,9 +3,16 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("dork",help='pass a full dork as an argument, remember quotes need to be escaped, for example allintext:\\"github.com\\"',type=str)
+args = parser.parse_args()
 
-base_url = "https://duckduckgo.com/?t=ffab&q=inurl:\"netspi\"&ia=web"
+print("Term searched for: %s" %args.dork)
+
+base_url = "https://duckduckgo.com/?t=ffab&q=%s&ia=web"%(args.dork)
+print("Full URL used in search: %s\n\n" % base_url)
 
 opts = Options()
 opts.set_headless()
@@ -14,10 +21,17 @@ browser = Firefox(executable_path="C:\\Users\\SamSepi0l\\Desktop\\geckodriver-v0
 browser.get(base_url)
 browser.implicitly_wait(5)
 
-browser.execute_script("document.getElementsByClassName('result--more__btn btn btn--full')[0].click()") # We can fix this later, but we need to iterate over the more results tab in the event that that element is present in the page more than once 
+
+# We can fix this later, but we need to iterate over the more results tab in the event that that element is present in the page more than once 
+try:
+	browser.execute_script("document.getElementsByClassName('result--more__btn btn btn--full')[0].click()")
+except Exception as e:
+	print('''Unable to scroll for more pages. This could mean that your search returned only 1 page of results
+or that something is blocking this script from scrolling further. Recommended that you search the dork
+term manually in a browser to verify this is not an error.\n\n''')
 
 results = browser.find_elements_by_class_name('result')
-
+print('RESULTS:\n')
 for i in range(len(results)):
 	print(results[i].text+'\n')
 browser.close()
