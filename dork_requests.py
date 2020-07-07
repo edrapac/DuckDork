f2e326dd0b6d8c96fadb112d7cb4c176d126dffb
edrapac <emerson@NETSPI-436-EDR.netspi.local>
2020-07-07 15:16:39 -0500

    makde adjustments for edge cases

--- dork_requests.py
+++ dork_requests.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 import sys
+from time import sleep 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 parser = argparse.ArgumentParser()
 parser.add_argument("dork",help='''pass a full dork as an argument, if you want exact searches ie you want the dork to use quotation marks, 
-pass the exact y arg and make sure to encapsulate your dork in single quotes. PLEASE NOTE THAT EXACT SEARCHES WITH A DORK OFTEN FAIL ie allintext:"mark zuckerburg" but exact searches ie "mark zuckerberg" do not\n''',type=str)
+pass the exact y. PLEASE NOTE THAT EXACT SEARCHES WITH A DORK OFTEN FAIL ie allintext:"mark zuckerburg" but exact searches ie "mark zuckerberg" do not\n''',type=str)
 parser.add_argument("results",help='number of pages of results you want back. Default is 1 page',const=0,nargs='?',type=int)
 parser.add_argument("exact",help='usage of quotes for exact searching',const='n',nargs='?',type=str)
 args = parser.parse_args()
@@ -16,11 +17,13 @@ postURL = 'https://html.duckduckgo.com/html/' #POSTS must go to this base url
 
  
 def get(dork):
-	if ':' in args.dork and args.exact=='y':
-		dork = args.dork.split(':')[0]+':'+'\"'+args.dork.split(':')[1]+'\"'
+	if args.exact=='y':
+		if ':' in args.dork:
+			dork = args.dork.split(':')[0]+':'+'\"'+args.dork.split(':')[1]+'\"'
+		else:
+			dork = '\"'+args.dork+'\"'
 	else:
 		dork=args.dork
-
 	mydict={'q':dork}
 	base_url = 'https://html.duckduckgo.com/html'
 	get_request=requests.get(base_url,headers=headers,params=mydict)
@@ -28,7 +31,12 @@ def get(dork):
 	
 	 # if you only want 1 page of results back
 	base_results=soup.find_all('a',{'class':'result__a'})
-	
+	if len(base_results)==0: # something messed up if this is empty
+		print('Some sort of error has just occurred, this might be but is not limited to a throttle duckduckgo has imposed on you for scraping their engine')
+		print('Returning response now...')
+		print(soup)
+		sys.exit(1)
+
 	for i in range(len(base_results)):
 		result_title = base_results[i].text
 		result_url = base_results[i].attrs['href']
@@ -48,6 +56,7 @@ def get(dork):
 	
 	else: #if a user wants more than a single page of results
 		try:
+
 			vqd=soup.find("input",{'name':'vqd'}).attrs['value']
 			q=soup.find("input",{'name':'q'}).attrs['value']
 			s=soup.find("input",{'name':'s'}).attrs['value'] 
@@ -60,6 +69,8 @@ def get(dork):
 			
 			#Make initial POST request and get back the blob of data we want 
 			for i in range(args.results):
+				sleep(1) # so we dont overload duckduckgo's garbo servers
+
 				x=requests.post(postURL,headers=headers,data=postParams)
 				results = BeautifulSoup(x.text,'lxml')
 				base_results=results.find_all('a',{'class':'result__a'})
