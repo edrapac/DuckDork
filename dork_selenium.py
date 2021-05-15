0dad0d84f5061965b8ac8d17e7d1eb9f4b9f73b0
Emerson Drapac <edrapac@netspi.com>
2021-05-15 09:42:05 -0500

    more selenium improvements

--- dork_selenium.py
+++ dork_selenium.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+import urllib.parse
 import argparse
 from time import sleep
 
@@ -11,14 +12,18 @@ parser.add_argument("results",help='number of pages of results you want back. De
 args = parser.parse_args()
 
 print("Term searched for: %s" %args.dork)
-base_url = "https://duckduckgo.com/html?t=ffab&q=%s&ia=web"%(args.dork)
+
+
+encoded_dork = urllib.parse.quote_plus(args.dork)
+base_url = "https://duckduckgo.com/?q=%s&t=hc&va=u&ia=web"%(encoded_dork)
+
 print("Full URL used in search: %s\n" % base_url)
 print("Searching, please be patient this may take a little while\n\n")
 
 opts = Options()
 opts.set_headless()
 assert opts.headless
-browser = Firefox(executable_path="path\\to\\gecko\\driver",options=opts)
+browser = Firefox(executable_path="/Users/edrapac/Documents/git/DuckDork/geckodriver",options=opts)
 browser.get(base_url)
 browser.implicitly_wait(5)
 
@@ -29,11 +34,12 @@ initial_results= browser.find_elements_by_class_name('result')
 for i in range(len(initial_results)):
 	results.append(initial_results[i].text)
 
-sleep(1.5)
+sleep(2)
 
+# Returns X amount of pages as specified with the results arg
 try:
 	for i in range(args.results):
-		browser.execute_script("document.getElementsByClassName('btn btn--alt')[0].click()")
+		browser.execute_script("document.getElementsByClassName('result--more__btn btn btn--full')[0].click()") # Finds the More Results button and clicks it
 		sleep(1.5)
 		temp_res = browser.find_elements_by_class_name('result')
 		for i in range(len(temp_res)):
