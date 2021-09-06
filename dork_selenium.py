138d838700860b560c92792dbf20f4cdfbb627e7
edrapac <e@ubuntu.com>
2021-09-06 10:20:25 -0500

    small updates

--- dork_selenium.py
+++ dork_selenium.py
@@ -4,7 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 import urllib.parse
 import argparse
+import datetime
 from time import sleep
+
+# TODO - Add DNSDumpster scraping if the dork is a URL/domain based dork
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dork",help='pass a full dork as an argument, remember quotes need to be escaped, for example allintext:\\"github.com\\"',type=str)
@@ -23,7 +26,7 @@ print("Searching, please be patient this may take a little while\n\n")
 opts = Options()
 opts.set_headless()
 assert opts.headless
-browser = Firefox(executable_path="PATH TO GECKODRIVER",options=opts)
+browser = Firefox(executable_path="./geckodriver",options=opts)
 browser.get(base_url)
 browser.implicitly_wait(5)
 
@@ -49,8 +52,13 @@ except Exception as e:
 	print('''Unable to scroll for more pages. This could mean that your search returned only 1 page of results
 or that something is blocking this script from scrolling further. Recommended that you search the dork
 term manually in a browser to verify this is not an error.\n\n''')
-
+browser.close()
+now = datetime.datetime.now()
+now_file_fmt = now.strftime('%Y-%m-%d_%H.%M.%S')+".log"
 print('RESULTS:\n')
+results_file = open(now_file_fmt,"w")
 for i in range(len(results)):
 	print(results[i]+'\n\n')
-browser.close()
+	results_file.write(results[i]+'\n\n')
+results_file.close()
+
